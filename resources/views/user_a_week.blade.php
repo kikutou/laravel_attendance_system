f6566de846d13b7cfc656a558525c52387d9165f
kinrai92 <ray920213@gmail.com>
2019-02-04 15:37:03 +0900

    csv

--- resources/views/user_a_week.blade.php
+++ resources/views/user_a_week.blade.php
@@ -88,7 +88,7 @@
     </div>
     <div class="row">
       <div class="col-sm text-center">
-        <a href="{{ route('get_create_csv') }}">先月の勤怠状況を確認しましたか？</a>
+        <a href="{{ route('get_create_csv') }}">本月の勤怠状況を確認しましたか？</a>
       </div>
     </div>
 @endsection
