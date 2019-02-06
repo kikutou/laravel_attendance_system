be6f2f81da34e2e460b32927cfe1884981296145
kinrai92 <ray920213@gmail.com>
2019-02-06 14:52:35 +0900

    merge source

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,7 +151,7 @@
             @if($errors->any())
               <div class="alert alert-danger">
                 @foreach($errors->all() as $error)
-                 <p>{{ $error }}</p>
+                 <div>{{ $error }}</div>
                 @endforeach
               </div>
             @endif
