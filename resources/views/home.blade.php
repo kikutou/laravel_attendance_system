45bf10caa6c0bfac12a5be1f83b2d1a7fe4fcc1c
attilaCadabra <hjwkky4869@gmail.com>
2019-02-06 17:52:29 +0900

    merge resource

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,13 +14,29 @@
         }
     </style>
     @if(!Auth::user()->admin_flg)
-      <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-8">
-                  @include("sub_view.info_card", ["infos" => Auth::user()->get_all_unread_infos()])
-              </div>
-          </div>
-      </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    @include("sub_view.info_card", ["infos" => Auth::user()->get_all_unread_infos()])
+                </div>
+            </div>
+        </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">名前:{{ Auth::user()->name }}</div>
+                    <div class="card-body">
+                      <ul>
+                        <li class="list-group-item">日付&nbsp:&nbsp{{ $today->format('Y年m月d日')}}</li>
+                        <li class="list-group-item">今月の遅刻回数&nbsp:&nbsp{{$late}}</li>
+                        <li class="list-group-item">今月の欠勤回数&nbsp:&nbsp{{$leave}}</li>
+                      </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
