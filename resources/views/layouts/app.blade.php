86614f69e70ea653bc41eb4561440b3912b2d93d
kinrai92 <ray920213@gmail.com>
2019-02-06 15:42:44 +0900

    merge source

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -141,11 +141,11 @@
                 }
             </style>
             @if(Session::has('message'))
-                <div class="alert alert-success">{{Session::get('message')}}</div>
+              <div class="alert alert-success">{{Session::get('message')}}</div>
             @endif
 
             @if(Session::has('error'))
-                <div class="alert alert-danger">{{Session::get('error')}}</div>
+              <div class="alert alert-danger">{{Session::get('error')}}</div>
             @endif
 
             @if($errors->any())
