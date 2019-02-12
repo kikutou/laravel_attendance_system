02791975cd0e27a018487d72982b98f2565f0931
attilaCadabra <hjwkky4869@gmail.com>
2019-02-12 10:35:44 +0900

    merge resource

--- resources/views/admin/all_info.blade.php
+++ resources/views/admin/all_info.blade.php
@@ -48,78 +48,74 @@
                      <tbody>
 
                      @if($all_infos)
-                       @foreach($all_infos as $one_info)
-                        <tr>
-                          <td class="td">
-                            <!-- メインモーダル -->
-                            <a href="#myModal{{ $one_info->id }}" data-toggle="modal" data-target="#myModal{{ $one_info->id }}">
-                              {{ $one_info->title }}
-                            </a>
-                            <div div class="modal fade" id="myModal{{ $one_info->id }}">
-                              <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h4 class="modal-title">{{ $one_info->title }}</h4>
-                                      <button id="subClose{{ $one_info->id }}" type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+                           @foreach($all_infos as $one_info)
+                            <tr>
+                              <td class="td">
+                                <!-- メインモーダル -->
+                                <a href="#myModal{{ $one_info->id }}" data-toggle="modal" data-target="#myModal{{ $one_info->id }}">
+                                  {{ $one_info->title }}
+                                </a>
+                                <div div class="modal fade" id="myModal{{ $one_info->id }}">
+                                  <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h4 class="modal-title">{{ $one_info->title }}</h4>
+                                          <button id="subClose{{ $one_info->id }}" type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                          <div style="text-align:left">{!! nl2br($one_info->comment) !!}</div>
+                                        </div>
+                                        <div class="modal-body">
+                                          @if(!$one_info->show_date->lt(\Carbon\Carbon::today()))
+                                            <button type="button" id="amend{{ $one_info->id }}" class="btn btn-primary" style="float:right;margin-top:15px" data-toggle="modal" data-target="#subModal{{ $one_info->id }}">修正</button>
+                                          @endif
+                                      </div>
+                                      <div class="modal-footer">
+                                        <div class="mr-auto">トータル&nbsp:
+                                            <!-- インナーモーダルの表示リンク -->
+                                            <a href="" data-toggle="modal" data-target="#inner0Modal{{ $one_info->id }}">
+                                                {{ count($one_info->get_users()) }}
+                                            </a>人
+                                        </div>
+                                        <div class="mr-auto">既読&nbsp:
+                                            <!-- インナーモーダルの表示リンク -->
+                                            <a href="" data-toggle="modal" data-target="#inner1Modal{{ $one_info->id }}">
+                                                {{ count($one_info->get_read_users()) }}/{{ count($one_info->get_users()) }}
+                                            </a>人
+                                        </div>
+                                        <div class="mr-auto">未読&nbsp:
+                                            <!-- インナーモーダルの表示リンク -->
+                                            <a href="" data-toggle="modal" data-target="#inner2Modal{{ $one_info->id }}">
+                                                {{ count($one_info->get_unread_users()) }}/{{ count($one_info->get_users()) }}
+                                            </a>人
+                                        </div>
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                      </div>
                                     </div>
-                                    <div class="modal-body">
-                                      <div style="text-align:left">{!! nl2br($one_info->comment) !!}</div>
-                                    </div>
-                                    <div class="modal-body">
-                                      @if(!$one_info->show_date->lt(\Carbon\Carbon::today()))
-                                        <button type="button" id="amend{{ $one_info->id }}" class="btn btn-primary" style="float:right;margin-top:15px" data-toggle="modal" data-target="#subModal{{ $one_info->id }}">修正</button>
-                                      @endif
-                                  </div>
-                                  <div class="modal-footer">
-                                    <div class="mr-auto">トータル&nbsp:
-                                        <!-- インナーモーダルの表示リンク -->
-                                        <a href="" data-toggle="modal" data-target="#inner0Modal{{ $one_info->id }}">
-                                            {{ count($one_info->get_users()) }}
-                                        </a>人
-                                    </div>
-                                    <div class="mr-auto">既読&nbsp:
-                                        <!-- インナーモーダルの表示リンク -->
-                                        <a href="" data-toggle="modal" data-target="#inner1Modal{{ $one_info->id }}">
-                                            {{ count($one_info->get_read_users()) }}/{{ count($one_info->get_users()) }}
-                                        </a>人
-                                    </div>
-                                    <div class="mr-auto">未読&nbsp:
-                                        <!-- インナーモーダルの表示リンク -->
-                                        <a href="" data-toggle="modal" data-target="#inner2Modal{{ $one_info->id }}">
-                                            {{ count($one_info->get_unread_users()) }}/{{ count($one_info->get_users()) }}
-                                        </a>人
-                                    </div>
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
                                   </div>
                                 </div>
-                              </div>
-                            </div>
-                            <!-- インナーモーダル（既読人数の詳細表示） -->
-                            <div class="modal fade" id="inner0Modal{{ $one_info->id }}">
-                                @include('sub_view.inner_modal',['title' => 'トータル','infos' => $one_info->get_users()])
-                            </div>
-                            <div class="modal fade" id="inner1Modal{{ $one_info->id }}">
-                                @include('sub_view.inner_modal',['title' => '既読','infos' => $one_info->get_read_users()])
-                            </div>
-                            <div class="modal fade" id="inner2Modal{{ $one_info->id }}">
-                                @include('sub_view.inner_modal',['title' => '未読','infos' => $one_info->get_unread_users()])
-                            </div>
-                            <!-- ここまで -->
-                            <!-- サブモーダル(お知らせの内容変更) -->
-                            <div div class="modal fade" id="subModal{{ $one_info->id }}">
-                                @include('sub_view.sub_modal',['one_info' => $one_info])
-                            </div>
-                            <!-- ここまで -->
-                          </td>
-                          <!-- ここまで -->
-                        <td class="td">{{ $one_info->show_date->format('Y-m-d') }}</td>
-                        <td class="td">{{ $one_info->created_at }}</td>
-                       </tr>
-                       @endforeach
-                       @else
-                         <tr>
-                             <td>{{ $no_date }}</td>
-                         </tr>
+                                <!-- インナーモーダル（既読人数の詳細表示） -->
+                                <div class="modal fade" id="inner0Modal{{ $one_info->id }}">
+                                    @include('sub_view.inner_modal',['title' => 'トータル','infos' => $one_info->get_users()])
+                                </div>
+                                <div class="modal fade" id="inner1Modal{{ $one_info->id }}">
+                                    @include('sub_view.inner_modal',['title' => '既読','infos' => $one_info->get_read_users()])
+                                </div>
+                                <div class="modal fade" id="inner2Modal{{ $one_info->id }}">
+                                    @include('sub_view.inner_modal',['title' => '未読','infos' => $one_info->get_unread_users()])
+                                </div>
+                                <!-- ここまで -->
+                                <!-- サブモーダル(お知らせの内容変更) -->
+                                <div div class="modal fade" id="subModal{{ $one_info->id }}">
+                                    @include('sub_view.sub_modal',['one_info' => $one_info])
+                                </div>
+                                <!-- ここまで -->
+                              </td>
+                              <!-- ここまで -->
+                            <td class="td">{{ $one_info->show_date->format('Y-m-d') }}</td>
+                            <td class="td">{{ $one_info->created_at }}</td>
+                           </tr>
+                           @endforeach
                        @endif
                      </tbody>
                   </table>
