da27184fe8cde5d6ac11e39e1b53dd571bbd6762
attilaCadabra <hjwkky4869@gmail.com>
2019-02-12 10:04:13 +0900

    merge resource

--- resources/views/admin/all_info.blade.php
+++ resources/views/admin/all_info.blade.php
@@ -46,6 +46,7 @@
                        </tr>
                      </thead>
                      <tbody>
+                     @if($all_infosgit)
                        @foreach($all_infos as $one_info)
                         <tr>
                           <td class="td">
@@ -114,6 +115,7 @@
                         <td class="td">{{ $one_info->created_at }}</td>
                        </tr>
                        @endforeach
+                     @endif
                      </tbody>
                   </table>
                 </div>
