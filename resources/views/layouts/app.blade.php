5cb7bd21a2f41ae76535b7f501b3d2e01f5fc56f
attilaCadabra <hjwkky4869@gmail.com>
2019-02-14 18:50:33 +0900

    merge resource

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
       <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
       <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
       <script src="https://code.highcharts.com/highcharts.js"></script>
+      <script src="{{ asset('/js/jquery-ja.js') }}"></script>
 
 
 
